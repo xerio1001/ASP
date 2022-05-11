df39c6ddd809a96cad0ff74ccf4fa92f436cc6c7
Dario Van Hasselt <dariovanhasselt@hotmail.be>
2022-05-11 13:30:10 +0200

    ProjectWeb

--- Project/Controllers/ProductController.cs
+++ Project/Controllers/ProductController.cs
@@ -37,7 +37,21 @@ namespace Project.Controllers
             {
                 return View(productService.GetProductBySupplier(id));
             }
-            
+        }
+
+        public ActionResult Order()
+        {
+            List<SupplierModel> supplierModel = new List<SupplierModel>();
+            List<SupplierModel> suppliers = supplierService.GetAllSuppliers();
+
+            foreach (SupplierModel supplier in suppliers)
+            {
+                supplierModel.Add(supplier);
+            }
+
+            ViewBag.displaySuppliers = supplierModel;
+
+            return View(productService.GetAllProducts());
         }
 
         // GET: ProductController/Details/5
@@ -108,6 +122,8 @@ namespace Project.Controllers
                 newProduct.AmountInStock = int.Parse(collection["AmountInStock"]);
                 newProduct.SupplierId = collection["SupplierId"];
                 newProduct.Barcode = collection["Barcode"];
+                newProduct.Ordered = collection["Ordered"].Contains("true");
+                //newProduct.Ordered = Convert.ToBoolean(collection["Ordered"].First()); <- Werkt ook.
 
                 productService.Update(id, newProduct);
 
